6d21847c469df3bf3e85af136f7fa57cfe695c4f
Mario Castillo <marcas2006@gmail.com>
2022-06-25 08:07:59 -0400

    Final commit

--- Exercise_2.py
+++ Exercise_2.py
@@ -9,3 +9,15 @@ T_c = np.array([[0.0, 0.1, 0.9, 0.0],
                 [0.9, 0.1, 0.0, 0.0],
                 [0.0, 0.0, 0.1, 0.9],
                 [0.9, 0.0, 0.0, 0.1]])
+
+Rsc = np.array([[0, 1, 0, 2]])
+
+Vp_0 = np.array([[0.0, 0.0, 0.0, 0.0]])
+
+Vp_1 = Rsc
+
+Vp_2 = Rsc + Rsc @ T_c.T
+
+print("Exercise 2A:", Vp_0.tolist())
+print("Exercise 2B: ", Vp_1.tolist())
+print("Exercise 2C:", Vp_2.tolist())
